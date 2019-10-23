05354ba44aea16b3e0ac09075df23db86f23dee6
Shawn Pyle <shawn.pyle@the-jci.org>
2019-10-23 09:00:18 -0400

    Remove example since its in the README

--- script/example.jsx
+++
@@ -1,8 +0,0 @@
-var rootPath = new File($.fileName).parent;
-$.evalFile(rootPath + '/node_modules/extendscript-logger/index.jsx');
-var logger = new Logger(rootPath+'/log/example.log');
-
-logger.debug('Debug message');
-logger.info([1, "Random string"]);
-logger.notice({foo:"bar"});
-logger.error("Crap! Something went wrong.");
