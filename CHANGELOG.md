d8f8bc6f7c50b1b0e21df6e0435255a6d5a2a1c8
Shawn Pyle <shawn.pyle@the-jci.org>
2020-03-12 14:33:35 -0400

    Use extendscript-json

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,12 @@ All notable changes to this project will be documented in this file.
  
 The format is based on [Keep a Changelog](http://keepachangelog.com/) and this project adheres to [Semantic Versioning](http://semver.org/).
 
+# 0.3.0 - 2020-03-12
+
+## Changed
+
+1. Swapped out `lib/json2.js` for [extendscript-json](https://github.com/theasci/extendscript-json).
+
 # 0.2.1 - 2020-03-03
 
 ## Changed
--- index.jsx
+++ index.jsx
@@ -4,8 +4,8 @@ Global.rootPath = Global.rootPath || (typeof(rootPath) != 'undefined' ? rootPath
 
 //Load dependencies
 $.evalFile(Global.rootPath + '/node_modules/extendscript-es5-shim/index.js');
+$.evalFile(Global.rootPath + '/node_modules/extendscript-json/index.jsx');
 $.evalFile(Global.rootPath + '/node_modules/moment/moment.js');
 
 //Load libraries
-$.evalFile(Global.extendscriptLoggerRootPath + '/lib/json2.js');
 $.evalFile(Global.extendscriptLoggerRootPath + '/src/Logger.jsx');
